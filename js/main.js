960f905a60364bad4352207e5c9a08984937a6bb
UmbertoDV <biccoumby2002@hotmail.com>
2023-01-20 13:49:11 +0100

    all passw

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 let name = prompt("Inserisci il tuo NOME");
 let surname = prompt("Inserisci il tuo COGNOME");
 let color = prompt("Inserisci il tuo COLORE PREFERITO");
+
+document.getElementById("all-passw").innerHTML = name + surname + color + "23";
