52c7ec57f3033d356213988800526588a6787aff
UmbertoDV <biccoumby2002@hotmail.com>
2023-01-20 15:43:23 +0100

    fix variables

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let name = prompt("Inserisci il tuo NOME");
-let surname = prompt("Inserisci il tuo COGNOME");
-let color = prompt("Inserisci il tuo COLORE PREFERITO");
+const nome = prompt("Inserisci il tuo NOME");
+const surname = prompt("Inserisci il tuo COGNOME");
+const color = prompt("Inserisci il tuo COLORE PREFERITO");
 
-document.getElementById("all-passw").innerHTML = name + surname + color + "23";
+document.getElementById("all-passw").innerHTML = `${nome}${surname}${color}23`;
